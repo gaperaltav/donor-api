499721d963aee1ad5a34b8c3344df572ce46d604
gaperaltav <gaperaltav@gmail.com>
2018-10-01 20:57:13 -0700

    Finishing donations route

--- routes/donations.js
+++ routes/donations.js
@@ -1,17 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const DonationRequest = require('../models/donationRequest');
 
-const donations = [
-  { title: 'I Need blood', reason: 'I have cancer', bloodType: 'A+' },
-  { title: 'I Need blood', reason: 'My mon is sick', bloodType: 'B+' },
-  { title: 'I Need blood', reason: 'I\'m sick', bloodType: 'A-' },
-  { title: 'I Need blood', reason: 'I need more blood', bloodType: 'O+' },
-  { title: 'I Need blood', reason: 'I aunt is sick', bloodType: 'AB+' }
-];
+router.get('/:id*?', (req, res, next) => {
 
-router.get('/', (req, res) => {
-  res.send(donations)
-})
+  const filter = req.params.id ? { _id: req.params.id } : {};
 
+  DonationRequest
+    .find(filter)
+    .then(donations => res.send(donations))
+    .catch(next);
+});
 
-module.exports = router
+router.post('/', (req, res, next) => {
+  DonationRequest
+    .create(req.body)
+    .then(donations => res.send(donations))
+    .catch(next);
+});
+
+router.put('/:id', function (req, res, next) {
+  DonationRequest
+    .findOneAndUpdate({ _id: req.params.id }, req.body)
+    .then(donation => res.send(donation))
+    .catch(next);
+});
+
+router.delete('/:id', function (req, res, next) {
+  DonationRequest
+    .findOneAndRemove({ _id: req.params.id })
+    .then(donation => res.send(`Donation '${donation._id}' was deleted correctly.`))
+    .catch(next);
+});
+
+module.exports = router;
