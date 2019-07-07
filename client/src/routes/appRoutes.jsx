6f9b845ec861bc576b1e688344bd6b34d319641e
gaperaltav <gaperaltav@gmail.com>
2019-07-07 02:41:17 -0400

    Adding react-router and react-router-dom and creating a basic app routes

---
+++ client/src/routes/appRoutes.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route } from 'react-router';
+import { Switch } from 'react-router-dom'
+import Home from '../pages/Home';
+
+const AppRoutes = () => {
+    return (
+        <Switch>
+            <Route exact path="/" component={Home} />
+        </Switch>
+    )
+}
+
+export default AppRoutes;
