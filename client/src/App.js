d906e17d8ae567f2e4c61165fb1a36966fb33827
gaperaltav <gaperaltav@gmail.com>
2019-07-11 00:15:24 -0400

    Reordening things and adding axios in the client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom'
+import AppRoutes from './routes/AppRoutes';
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Donor app's client!
-      </div>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
     );
   }
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
-import AppRoutes from './routes/appRoutes';
+import App from './App';
 
-ReactDOM.render(
-    <BrowserRouter >
-        <AppRoutes />
-    </BrowserRouter>,
-    document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
