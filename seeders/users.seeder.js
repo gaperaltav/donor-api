889faf0e692b8886bb828a3dc9504f9b5fd405ae
gaperaltav <gaperaltav@gmail.com>
2019-07-14 20:50:06 -0400

    Adding default _id values in user.seeder

--- seeders/users.seeder.js
+++ seeders/users.seeder.js
@@ -1,8 +1,10 @@
 var Seeder = require('mongoose-data-seed').Seeder;
 var Model = require('../src/models/user');
+const ObjectId = require('mongodb').ObjectID;
 
 var data = [
   {
+    _id: new ObjectId('5d2bc042d4e3bb2ca9554daf'),
     firstName: 'Gabriel',
     lastName: 'Peralta',
     email: 'dfadf@gmail.com',
@@ -10,6 +12,7 @@ var data = [
     bloodType: 'A+',
   },
   {
+    _id: new ObjectId('5d2bc2680ef58a2f8a24d438'),
     firstName: 'jhon',
     lastName: 'Doe',
     email: 'jhondoe@gmail.com',
@@ -17,6 +20,7 @@ var data = [
     bloodType: 'O+',
   },
   {
+    _id: new ObjectId('5d2bc2eac1fd9c30f449edb6'),
     firstName: 'Sarah',
     lastName: 'Connor',
     email: 'sarahconnor@gmail.com',
@@ -24,6 +28,7 @@ var data = [
     bloodType: 'AB+',
   },
   {
+    _id: new ObjectId('5d2bc2eac1fd9c30f449edb7'),
     firstName: 'Jhon',
     lastName: 'Connor',
     email: 'jhonconnor@gmail.com',
