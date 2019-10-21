3d2bc71c18c4088b9c1ea264811621d8c99b797e
gaperaltav <gaperaltav@gmail.com>
2019-10-21 14:14:29 -0400

    Adding redux thunk and logger to the store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,9 +1,15 @@
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import reducers from './reducers';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+const composedEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composedEnhancer(
+    applyMiddleware(thunk, logger),
+  )
 );
 
 export default store;
