5d8741449e383febb18586cd4878214dc9fa5570
gaperaltav <gaperaltav@gmail.com>
2019-10-28 18:39:59 -0400

    Adding babel-eslint like a parser in eslintrs.js and adding some lintter scripts in package.json

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -19,13 +19,14 @@ module.exports = {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
     },
+    "parser": "babel-eslint",
     "parserOptions": {
         "ecmaFeatures": {
             "jsx": true
         },
         "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
+        "sourceType": "module",
+    }, 
     "plugins": [
         "react",
     ],
